35d3c1455021bb945e2b3efc893a628c15b73369
Vebnik(Nik) <vebnik111@gmail.com>
2022-05-22 18:26:43 +0300

    add google translate logic

--- logic/googleApiTranslate/requestApi.js
+++ logic/googleApiTranslate/requestApi.js
@@ -1,23 +1,19 @@
 const fetch = require('node-fetch')
 const { transUrl, transBody } = require('./urlParse')
 
+
 const getTrans = (text) => {
 
 	fetch(transUrl().toString(), {
-		headers: {
-			"content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-			"x-goog-batchexecute-bgr": "[\";a3W4dTjQAAYI1eB4KNVfFS2CSBTKaNgmACkAIwj8RjBo2dwAtgfLpdstX6XevmpFve8GbRW-JJ5mRNwjqf1vcTbYLB8AAABnTwAAAAJ1AQcXALgp0dMZHVkHXTiJGr7Uw4QqVq3rG1vc8XRmHAioabjfwIzqE35YCYmjfCohO9ulhInvFFAW2HoTxuFYRaAlOi-W-Q_KaQaD_X1zD8HLS5zJlYFlE6uEGjbwJ8V9VaWZVSE3j3LucoYb0an-mGDmq7DXdlPFFVFn8pc7upCTERZWOmbNMQXAdD_JlFe9pWYQBwk7Pfr0nX_dwGsCqpzQD_j0ycDKBhzruO-cy2OSpPLcGSS7zxEUvBp4hAJuIlX-J5Nx2RaJVxN86yOnwfqHcZVvd9mDjAfZL_DUWXqk2yO7YMMp4jCql4ssVqDWC_HPbjI0wBylfXhaaa7Xux7kyN3Ife47ZkMvystkFrHh2yYtowB1zdD4ZCNrWWckPHWk8Qd9I-AQGdMYueTFL_TFV47yEVX-rW8YQgv3ZRD9XqX0d24kzY0nSGXm4TQnVJbRCB3rKLoxJRGZsUhRlFdeTwMYvDkwxFZPt--1nXemDKenijqOH2cMRHbUdOx7FcLkxcMXihNacOK6vBgG11lapNA77Lc7URY_NxIxVAidKRfsWIuUgu2VlrXr5wddIdxQQtOjTQ3n0JBhRoVN82_f_grCwopLYigxxGNLonpQacM8DY6mnR4-pUzEQEHeGUIPklCp5WUwVnguppmIAK00hKXaYHzkGmceeWDRHJETgb0VmhQlqB3RXOcChvQ78P3Y-Xm52X4lvM4nLS2EA-X-fmyRKh6jF09hjX1TSFQ5QwsHG0c7bPGXsP1YvSjU-uzz3UVCNRBt-005xN3fDV3FqoYeYfK5MYIJkN_kLWrzhLulYnLCw87Rv0wEIATBmsNElbLWoWngXp0R66HinT3eMrSU8FRtm2VKKly23OiOMdtlMVb_HfPX2BkkJVPRjJ8Gj7JtKmkdSWOkkZuBbswb8wsVWim5a9DZtXHKHGP3uw_hv5th6EURDZp7piicYP6_Hq7_uoQzDiNBrZP5ypJnlT_nfK7yLyE9GrSz1QIRaW2pNpoZsAVQz8IcKVlWixsHtt4yDIRg2wV8iQCNlb_PLdY2VOMPzsZQJtFJvxsrQ3R-sa_Cko9N9Bk7Cg\",null,null,123,null,null,null,0,\"2\"]",
-			"x-same-domain": "1"
-		},
-		referrer: "https://translate.google.com/",
-		referrerPolicy: "origin",
+		headers: { "content-type": "application/x-www-form-urlencoded;charset=UTF-8"},
 		body: transBody(text),
 		method: "POST",
 	})
 		.then(res => res.ok ? res.text() : console.error(res))
-		.then(data => console.log(data))
+		.then(data => {
+			const parsArr = data.match(/\[\[([\s\S]+?)]]/g)
+			console.log(parsArr)
+		})
 }
 
-getTrans('')
-
-
+getTrans('Shield self and nearby enemy')
---
+++ logic/parsingJson.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const path = require('path')
+
+
+const getText = async () => {
+
+	const data = new Promise(resolve => {
+		fs.readFile(path.join('..', 'translated', 'English.json'), (err, data) => {
+			if (err) return console.error(err)
+			resolve(data)
+		})
+	})
+
+	data.then(el => {
+		const textStr = JSON.parse(el).Nodes.map(text => text.Text)
+
+		fs.writeFile(path.join('..', 'translated', 'EnglishText.txt'), textStr.join('|'), err => err ? console.error(err) : null)
+	})
+}
+
+const getRuText = async () => {
+
+	const data = new Promise(resolve => {
+		fs.readFile(path.join('..', 'translated', 'vRisingRu.txt'), 'utf-8', (err, data) => {
+			if (err) return console.error(err)
+			resolve(data)
+		})
+	})
+
+	const dataEn = new Promise(resolve => {
+		fs.readFile(path.join('..', 'translated', 'English.json'), 'utf-8', (err, data) => {
+			if (err) return console.error(err)
+			resolve(data)
+		})
+	})
+
+	dataEn.then(async el => {
+
+		await data.then(ruText => {
+			return ruText.split('|').map(el => el.replace(/\r\n/g, ''))
+		})
+			.then(async text => {
+				const ruParsingText = JSON.parse(el).Nodes.map((nodeText, i) => {
+					return {
+						"Guid": nodeText.Guid,
+						"Text": text[i]
+					}
+				})
+
+				await fs.writeFile(path.join('..', 'translated', 'RuTextV2.json'), JSON.stringify(ruParsingText), err => err ? console.error(err) : null)
+			})
+
+	})
+}
+
+getRuText().catch(err => console.error(err ))
+//getText().catch(err => console.error(err ))
